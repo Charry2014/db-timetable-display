3e29d5657eac93fc1e9920f6767faab964e994b4
Charry2014 <58067238+Charry2014@users.noreply.github.com>
2024-09-20 17:16:46 +0200

    Wrap API communications better

--- bahnapi.py
+++ bahnapi.py
@@ -19,15 +19,22 @@ class TransportAPI:
         self.__duration = duration
 
 
+    def __communicate(self, url):
+        response = requests.get(f'https://v6.db.transport.rest/stations?query={self.__name}')
+        if response.status_code != 200:
+            print(f'Server error code {response.status_code}')
+            raise SystemExit(1)
+
+        updated = datetime.strftime(datetime.now(), '%H:%M')
+        return response, updated
+
     def __get_station(self) -> dict:
         '''Get station details from the transport API using the station name.
         The get() returns a dict with the station details, keyed on station
         ID. This is unhelpful to us as we don't know the ID yet. We save the
         station object by accessing the first (and only) element in the dict.
         '''
-        response = requests.get(f'https://v6.db.transport.rest/stations?query={self.__name}')
-        if response.status_code != 200:
-            raise Exception(f'Server error code {response.status_code}')
+        response, _ = self.__communicate(f'https://v6.db.transport.rest/stations?query={self.__name}')
         self.__stations = response.json()
         if not len(self.__stations) == 1:
             raise Exception('Station name not unique or not found')
@@ -36,10 +43,7 @@ class TransportAPI:
 
 
     def get_departure_details(self, id: int):
-        response = requests.get(f'https://v6.db.transport.rest/stops/{id}/departures?results={self.__departures}&duration={self.__duration}&bus=false&taxi=false')
-        if response.status_code != 200:
-            raise Exception(f'Server error code {response.status_code}')
-            exit(response.status_code)
+        response, timestamp = self.__communicate(f'https://v6.db.transport.rest/stops/{id}/departures?results={self.__departures}&duration={self.__duration}&bus=false&taxi=false')
         decoded = response.json()['departures']
         # The response is a list of dicts with keys direction (Tutzing:str) delay (seconds:int)
         # when ('2024-09-18T16:23:00+02:00':str) and plannedWhen ('2024-09-18T16:23:00+02:00':str)
@@ -64,7 +68,7 @@ class TransportAPI:
                 delay = 0 
             filtered.append((d['direction'], depart_in, when, delay, planned))
 
-        return filtered
+        return filtered, timestamp
 
 
     @property
@@ -85,12 +89,12 @@ class Station:
     def get_departure_details(self):
         ''''Grafing Bahnhof', (Depart in) 8, '17:40', (Delay) 32, '17:08'
         '''
-        trains = self.__transport_api.get_departure_details(self.id)
+        trains, timestamp = self.__transport_api.get_departure_details(self.id)
         trains.sort(key=lambda x: x[1])
         trains_east = [train for train in trains if train[0] in ['Ebersberg(Oberbay)', 'Grafing Bahnhof']]
         trains_west = [train for train in trains if train[0] not in ['Ebersberg(Oberbay)', 'Grafing Bahnhof']]
 
-        retval = json.dumps({"timestamp": f"Updated {datetime.strftime(datetime.now(), '%H:%M') }",
+        retval = json.dumps({"timestamp": f"Updated {timestamp}",
                            "trains_east": trains_east, "trains_west": trains_west})
         return retval
 
