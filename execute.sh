6bc03e2bca277d92f0f1fd94342b9ed35b356642
Toby Martin <toby.martin@loewe.de>
2025-10-31 09:16:04 +0100

    pip3

--- execute.sh
+++ execute.sh
@@ -6,7 +6,7 @@ apt-get install -y curl git python3 python3-pip # python3-venv
 #python3 -m venv venv
 #source venv/bin/activate
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 waitress-serve --listen=0.0.0.0 trains:app
 
 
