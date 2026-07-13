c2f71b72b54771bf875fb917b8b3ed820036a633
Toby Martin <toby.martin@loewe.de>
2026-07-13 12:15:03 +0200

    Again 403 fix - change headers

--- transportapi.py
+++ transportapi.py
@@ -12,12 +12,26 @@ class TransportAPI:
         Returns a tuple with the response and the time of the request if successful.
         If the request fails then we return the status code. and time of the request.
         '''
+        '''
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language': 'en-US,en;q=0.5',
             'Accept-Encoding': 'gzip, deflate',
             'Connection': 'keep-alive',
+        }
+        '''
+        headers = {
+            "User-Agent": (
+                "Mozilla/5.0 (X11; Linux x86_64) "
+                "AppleWebKit/537.36 "
+                "(KHTML, like Gecko) Chrome/137 Safari/537.36"
+            ),
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Language": "de-DE,de;q=0.9",
+            "Referer": "https://www.bahn.de/",
+            "Origin": "https://www.bahn.de",
+            "Connection": "keep-alive",
         }
         logger.debug(f"Communicating with {url}")
         response = requests.get(url, headers=headers)
