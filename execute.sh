0c7541d5b2a6c2f84ada303ca45dae9546fa3305
Toby Martin <toby.martin@loewe.de>
2026-07-13 18:11:44 +0200

    fix: 5123:8180

--- execute.sh
+++ execute.sh
@@ -7,6 +7,6 @@ python3 -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
-exec waitress-serve --listen=0.0.0.0:8080 trains:app
+exec waitress-serve --listen=0.0.0.0:8180 trains:app
 
 
