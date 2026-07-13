11c61836c1d630aea77631be33389aa43934b223
Toby Martin <toby.martin@loewe.de>
2026-07-13 17:10:26 +0200

    fix: port to 8080

--- execute.sh
+++ execute.sh
@@ -7,6 +7,6 @@ python3 -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
-waitress-serve --listen=0.0.0.0 trains:app
+exec waitress-serve --listen=0.0.0.0:8080 trains:app
 
 
