ae70662d0a688599fee3a9b476f480f922b585c8
Toby Martin <toby.martin@loewe.de>
2026-07-26 18:40:42 +0200

    fix: 403 with Playwright

--- execute.sh
+++ execute.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 apt-get update -y && apt-get upgrade -y
-apt-get install -y curl git python3 python3-pip python3-venv
+apt-get install -y curl git python3 python3-pip python3-venv wget gnupg
 
 python3 -m venv venv
 source venv/bin/activate
+
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | \
+    gpg --dearmor -o /usr/share/keyrings/google-linux-signing-keyring.gpg
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-linux-signing-keyring.gpg] \
+https://dl.google.com/linux/chrome/deb/ stable main" | \
+tee /etc/apt/sources.list.d/google-chrome.list
+
+apt-get update -y
+apt-get install -y google-chrome-stable
 
 pip install -r requirements.txt
 exec waitress-serve --listen=0.0.0.0:8180 trains:app
--- transportapi.py
+++ transportapi.py
@@ -8,6 +8,10 @@ class TransportAPI:
     # This site has bad IPv6 support. Turn it off.
     # requests.packages.urllib3.util.connection.HAS_IPV6 = False
 
+    def __init__(self):
+        self.__session = requests.Session()
+        self.__session.impersonate = "chrome150"
+
     def communicate(self, url):
         '''Handle communication with the server - if the server responds 200 then all good.
         If the server responds anything else then we assume the call failed.
@@ -24,32 +28,32 @@ class TransportAPI:
         }
         '''
         headers = {
-            "User-Agent": (
-                "Mozilla/5.0 (X11; Linux x86_64) "
-                "AppleWebKit/537.36 "
-                "(KHTML, like Gecko) Chrome/137 Safari/537.36"
-            ),
             "Accept": "application/json, text/plain, */*",
             "Accept-Language": "de-DE,de;q=0.9",
             "Referer": "https://www.bahn.de/",
             "Origin": "https://www.bahn.de",
-            "Connection": "keep-alive",
         }
         logger.debug(f"Communicating with {url}")
         # response = requests.get(url, headers=headers)
-        response = requests.get(
+        response = self.__session.get(
             url,
-            headers=headers,
-            impersonate="chrome136"
+            headers=headers
         )
         updated = datetime.strftime(datetime.now(), '%H:%M')
         logger.debug(f"Communicating ended - response code {response.status_code} at {updated}")
+
 
         if response.status_code == 200:
             # If the response is 200 OK then we assume the server returned a JSON object 
             retval = response.json()
         else:
             logger.error(f'Server returned unexpected code {response.status_code}.')
+            # logger.error(f"Response text: {curl_cffi.__version__}")
+            for k, v in response.headers.items():
+                logger.error(f"{k}: {v}")
+            # for c in self.__session.cookies:
+            #     logger.error(f"{c.name} = {c.value}")
+            
             retval = response.status_code
 
         return retval, updated
